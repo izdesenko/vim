371353c4bdcc3ac9f59be5c5651df8bed19fbce4
Ilya Zdesenko <igzdesenko@rt-dc.ru>
2021-07-09 21:45:44 +0300

    new emacs conf

--- init.el
+++ init.el
@@ -43,6 +43,12 @@
  '(cperl-autoindent-on-semi nil)
  '(find-file-suppress-same-file-warnings t)
  '(global-linum-mode t)
+ '(grep-command "grep -rl ")
+ '(grep-find-ignored-directories
+   '("SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "node_modules" "tmp" "*.swp" "log"))
+ '(grep-find-ignored-files
+   '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "*.swp"))
+ '(grep-search-path '(nil "~/"))
  '(indent-tabs-mode nil)
  '(linum-format "%4d ")
  '(package-selected-packages '(magit expand-region iedit))
@@ -53,7 +59,7 @@
  '(truncate-lines nil)
  '(uniquify-buffer-name-style 'forward nil (uniquify))
  '(uniquify-min-dir-content 2)
- '(word-wrap nil))
+ '(word-wrap t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
